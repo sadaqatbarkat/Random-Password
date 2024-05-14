62ea2dcad6cca520449a04d2811608dd17393193
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-05-14 09:30:18 +0500

    add index css and app.js files

--- app.js
+++ app.js
@@ -1,174 +1,632 @@
-var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-var symbols = "!@#$%&*()_<>/"
-var numbers = "1234567890"
-
-var password1 = ""
-var password2 = ""
-var password3 = ""
 
 
 
 
+function abc (){
+
+  
 
 
 
-
-
-
-
-
-
-
-function abc() {
+  var UpSet =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var password =  ""
+  var upCheckbox = document.getElementById("uppercase")
+  var len =  document.getElementById("range").value
+  if (upCheckbox.checked){
+	for (var i = 1; i <=len;i++){
+		random = Math.floor(Math.random()*UpSet.length)
+		password += UpSet[random]
 
 	
-	var upSet = document.getElementById("uppercase")
-	if (upSet.checked) {
-
-
-		for (var i = 0; i <= document.getElementById("range").value; i++) {
-
-			randomValues = Math.floor(Math.random() * letters.length)
-			password1 += letters[randomValues]
-
-
-
-
-
-		}
-		
-
-		var value = document.getElementById("range").value
-
-		document.getElementById("counting").innerText = value
-		document.getElementById("input").value = password1
 	}
 
-
-	var upSet = document.getElementById("uppercase");
-	if (upSet.checked) {
-
-
-		for (var i = 1; i <= document.getElementById("range").value; i++) {
-
-			randomValues = Math.floor(Math.random() * upper.length)
-			password1 += upper[randomValues]
-
-
-
-
-
-		}
-
-		var value = document.getElementById("range").value
-
-		document.getElementById("counting").innerText = value
-		document.getElementById("input").value = password1
-
-
-	}
-
-	var lowSet = document.getElementById("lowercase")
-	if (lowSet.checked) {
-
-
-		for (var i = 1; i <= document.getElementById("range").value; i++) {
-
-			randomValues = Math.floor(Math.random() * lowerCase.length)
-			password2 += lowerCase[randomValues]
-
-
-
-
-
-		}
-
-		var value = document.getElementById("range").value
-
-		document.getElementById("counting").innerText = value
-		document.getElementById("input").value = password2
-
-
-	}
-
-
-	var numSet = document.getElementById("numbers")
-	if (numSet.checked) {
-
-
-		for (var i = 1; i <= document.getElementById("range").value; i++) {
-
-			randomValues = Math.floor(Math.random() * numbers.length)
-			password3 += numbers[randomValues]
-
-
-
-
-
-	 	}
-
-		var value = document.getElementById("range").value
-
-		document.getElementById("counting").innerText = value
-		document.getElementById("input").value = password3
-	}
-
-
-	var symbolsSet = document.getElementById("symbols")
-
-
-
-
-	if (symbolsSet.checked) {
-
-
-		for (var i = 1; i <= document.getElementById("range").value; i++) {
-
-			randomValues = Math.floor(Math.random() * symbols.length)
-			password3 += symbols[randomValues]
-
-
-
-
-
-		}
-
-
-
-
-
-	}
-
-	var value = document.getElementById("range").value
-
-	document.getElementById("counting").innerText = value
-	document.getElementById("input").value = password3
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        document.getElementById("input").value  = password
+  }
+ // UpperCase End 
+
+
+ var lowSet =  "abcdefghijklmnopqrstuvwxyz"
+ var password =  ""
+   var upCheckbox = document.getElementById("lowercase")
+   var len =  document.getElementById("range").value
+   if (upCheckbox.checked){
+   for (var i = 1; i <=len;i++){
+     random = Math.floor(Math.random()*lowSet.length)
+     password += lowSet[random]
+ 
+   
+   }
+ 
+         document.getElementById("input").value  = password
+   }
+
+// lowerCase End
+
+var spaecialCharacter =  "!@#$%&()_<>"
+var password =  ""
+  var upCheckbox = document.getElementById("symbols")
+  var len =  document.getElementById("range").value
+  if (upCheckbox.checked){
+  for (var i = 1; i <=len;i++){
+    random = Math.floor(Math.random()*spaecialCharacter .length)
+    password += spaecialCharacter [random]
+
+  
+  }
+
+        document.getElementById("input").value  = password
+  }
+// End Symbols
+
+var num =  "1234567890"
+var password =  ""
+  var upCheckbox = document.getElementById("numbers")
+  var len =  document.getElementById("range").value
+  if (upCheckbox.checked){
+  for (var i = 1; i <=len;i++){
+    random = Math.floor(Math.random()*num .length)
+    password += num [random]
+
+  
+  }
+
+        document.getElementById("input").value  = password
+  }
+// End Numbers
+
+
+var upLow =  "ABbacdDCfEFejGhJHgIkLKiMnoNOmPrpRqStsTQUvwyVWxZXzY";
+var password =  ""
+  var upCheckbox = document.getElementById("uppercase")
+  var upCheckbox1 = document.getElementById("lowercase")
+  var len =  document.getElementById("range").value
+  if (upCheckbox.checked && upCheckbox1.checked){
+  for (var i = 1; i <=len;i++){
+    random = Math.floor(Math.random()*upLow .length)
+    password += upLow [random]
+
+  
+  }
+
+        document.getElementById("input").value  = password
+  }
+  // End UpperLowerCase
+
+  
+var upNumbers =  "A1B2C3D4E5F6G7H8I9J0K0L1M2N3O4P5Q6R7S8T9U0V6W5X2Y1Z0";
+var password =  ""
+  var upCheckbox = document.getElementById("uppercase")
+  var upCheckbox1 = document.getElementById("numbers")
+  var len =  document.getElementById("range").value
+  if (upCheckbox.checked && upCheckbox1.checked){
+  for (var i = 1; i <=len;i++){
+    random = Math.floor(Math.random()*upNumbers .length)
+    password += upNumbers [random]
+
+  
+  }
+
+        document.getElementById("input").value  = password
+  }
+  // End UppercaseNumbers
+
+  var upSymbols =  "A!B@C#D$E%F^G&H*I(J)K_L=M/N?O>P<QR\S<T>U@V#W%X$Y!Z_";
+  var password =  ""
+    var upCheckbox = document.getElementById("uppercase")
+    var upCheckbox1 = document.getElementById("symbols")
+
+    var len =  document.getElementById("range").value
+    if (upCheckbox.checked && upCheckbox1.checked){
+    for (var i = 1; i <=len;i++){
+      random = Math.floor(Math.random()*upSymbols .length)
+      password += upSymbols [random]
+  
+    
+    }
+  
+          document.getElementById("input").value  = password
+    }
+
+// End UpperCaseSymbols
+
+
+var lowSymbols =  "a!b@c#d$e%^fg&h*i(j)k_l=m/n?o>p<qr\s<t>u@v#w%x$y!z_";
+var password =  ""
+  var upCheckbox = document.getElementById("lowercase")
+  var upCheckbox1 = document.getElementById("symbols")
+  var len =  document.getElementById("range").value
+  if (upCheckbox.checked && upCheckbox1.checked){
+  for (var i = 1; i <=len;i++){
+    random = Math.floor(Math.random()*lowSymbols .length)
+    password += lowSymbols [random]
+
+  
+  }
+
+        document.getElementById("input").value  = password
+  }
+
+// End lowerCaseSymbols
+
+// var lowNumbers =  "a1b2c3d4e56g7h8i9j0k1l2m3n4o5p6r7s8t9u0v1w2x3y4z5";
+// var password9 =  ""
+//   var upCheckbox9 = document.getElementById("lowercase")
+//   var upCheckbox0 = document.getElementById("numbers")
+//   var len =  document.getElementById("range").value
+//   if (upCheckbox9.checked && upCheckbox0.checked){
+//   for (var i = 1; i <=len;i++){
+//     random = Math.floor(Math.random()*lowNumbers.length)
+//     password9 += lowNumbers [random]
+
+  
+//   }
+
+//         document.getElementById("input").value  = password9
+//   }
+
+  // End lowerCaseNumbers
+
+
+  var upLowNum =  "A102034405b6C7d8E9f0GhIjkLmNoPQrStUuVwXyz";
+  var password =  ""
+  var upCheckbox = document.getElementById("lowercase")
+    var upCheckbox1 = document.getElementById("uppercase")
+    var upCheckbox2 = document.getElementById("numbers")
+    var len =  document.getElementById("range").value
+    if (upCheckbox.checked && upCheckbox1.checked && upCheckbox2.checked){
+    for (var i = 1; i <=len;i++){
+      random = Math.floor(Math.random()*upLowNum .length)
+      password += upLowNum  [random]
+  
+    
+    }
+  
+          document.getElementById("input").value  = password
+    }
+ 
+    // End UppercaseLowerCaseNumbers
+   
+
+    
+  var upLowSym =  "!A@bC#dE%eF&gH&iJ(kL)mN?nO/pQ<rStUvWxYz>";
+  var password =  ""
+  var upCheckbox = document.getElementById("uppercase")
+    var upCheckbox1 = document.getElementById("lowercase")
+    var upCheckbox2 = document.getElementById("symbols")
+    var len =  document.getElementById("range").value
+    if (upCheckbox.checked && upCheckbox1.checked && upCheckbox2.checked){
+    for (var i = 1; i <=len;i++){
+      random = Math.floor(Math.random()*upLowSym .length)
+      password += upLowSym  [random]
+  
+    
+    }
+  
+          document.getElementById("input").value  = password
+    }
+
+// End UpperCaseLowerCaseSymbols
+
+
+
+``
+
+     
+  var symNum =  "<0!1@2#3$4$5%6^7&8(9)0_/>";
+  var password =  ""
+  
+    var upCheckbox = document.getElementById("numbers")
+    var upCheckbox2 = document.getElementById("symbols")
+    var len =  document.getElementById("range").value
+    if (upCheckbox.checked && upCheckbox2.checked ){
+    for (var i = 1; i <=len;i++){
+      random = Math.floor(Math.random()*symNum .length)
+      password += symNum  [random]
+  
+    
+    }
+  
+          document.getElementById("input").value  = password
+    }  
+ 
+   
+// var lowSymNum =  "1a!b2c@d3e#f4g$h5i%j6k&lm7n(op8q(rs)tu9v/w<xyz>";
+
+// var password =  ""
+// var upCheckbox = document.getElementById("lowercase")
+// var upCheckbox1 = document.getElementById("numbers")
+//  var upCheckbox2 = document.getElementById("symbols")
+//   var len =  document.getElementById("range").value
+//   if (upCheckbox.checked && upCheckbox1.checked && upCheckbox2.checked ){
+//   for (var i = 1; i <=len;i++){
+//     random = Math.floor(Math.random()*lowSymNum .length)
+//     password += lowSymNum  [random]
+
+  
+//   }
+
+//         document.getElementById("input").value  = password
+//   }
+
+   
+ 
+    var upLowNum =  "A102034405b6C7d8E9f0GhIjkLmNoPQrStUuVwXyz";
+    var password =  ""
+    var upCheckbox = document.getElementById("lowercase")
+      var upCheckbox1 = document.getElementById("uppercase")
+      var upCheckbox2 = document.getElementById("numbers")
+      var len =  document.getElementById("range").value
+      if (upCheckbox.checked && upCheckbox1.checked && upCheckbox2.checked){
+      for (var i = 1; i <=len;i++){
+        random = Math.floor(Math.random()*upLowNum .length)
+        password += upLowNum  [random]
+    
+      
+      }
+    
+            document.getElementById("input").value  = password
+      }
+      // var allCharacters =  "1A!b2C@d3E#f5G%h7I&j8K(l9M)n0o_P/Q>r<StUvWxYz";
+      // var password =  ""
+      // var upCheckbox = document.getElementById("uppercase")
+      //   var upCheckbox1 = document.getElementById("lowercase")
+      //   var upCheckbox2 = document.getElementById("symbols")
+      //   var upCheckbox3 = document.getElementById("numbers")
+      //   var len =  document.getElementById("range").value
+      //   if (upCheckbox.checked && upCheckbox1.checked && upCheckbox2.checked && upCheckbox3.checked){
+      //   for (var i = 1; i <=len;i++){
+      //     random = Math.floor(Math.random()*allCharacters.length)
+      //     password += allCharacters   [random]
+      
+        
+      //   }
+      
+      //         document.getElementById("input").value  = password
+      //   }
+
+        var lowNumbers =  "a1b2c3d4e56g7h8i9j0k1l2m3n4o5p6r7s8t9u0v1w2x3y4z5";
+        var password9 =  ""
+          var upCheckbox9 = document.getElementById("lowercase")
+          var upCheckbox0 = document.getElementById("numbers")
+          var len =  document.getElementById("range").value
+          if (upCheckbox9.checked && upCheckbox0.checked){
+          for (var i = 1; i <=len;i++){
+            random = Math.floor(Math.random()*lowNumbers.length)
+            password9 += lowNumbers [random]
+        
+          
+          }
+        
+                document.getElementById("input").value  = password9
+          }
+
+
+          var lowSymNum =  "1a!b2c@d3e#f4g$h5i%j6k&lm7n(op8q(rs)tu9v/w<xyz>";
+
+          var password =  ""
+          var upCheckbox = document.getElementById("lowercase")
+          var upCheckbox1 = document.getElementById("numbers")
+           var upCheckbox2 = document.getElementById("symbols")
+            var len =  document.getElementById("range").value
+            if (upCheckbox.checked && upCheckbox1.checked && upCheckbox2.checked ){
+            for (var i = 1; i <=len;i++){
+              random = Math.floor(Math.random()*lowSymNum .length)
+              password += lowSymNum  [random]
+          
+            
+            }
+          
+                  document.getElementById("input").value  = password
+            }
+
+
+            var allCharacters =  "1A!b2C@d3E#f5G%h7I&j8K(l9M)n0o_P/Q>r<StUvWxYz";
+            var password =  ""
+            var upCheckbox = document.getElementById("uppercase")
+              var upCheckbox1 = document.getElementById("lowercase")
+              var upCheckbox2 = document.getElementById("symbols")
+              var upCheckbox3 = document.getElementById("numbers")
+              var len =  document.getElementById("range").value
+              if (upCheckbox.checked && upCheckbox1.checked && upCheckbox2.checked && upCheckbox3.checked){
+              for (var i = 1; i <=len;i++){
+                random = Math.floor(Math.random()*allCharacters.length)
+                password += allCharacters   [random]
+            
+              
+              }
+            
+                    document.getElementById("input").value  = password
+              }
+                
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var slider = document.getElementById("range");
+
+// Add event listener for slider value change
+slider.addEventListener("input", function() {
+    // Get the current value of the slider
+    var value = parseInt(range.value);
+
+    // Log the current value to console (you can replace this with your desired functionality)
+    document.getElementById("counting").innerHTML =  value
+   
+});
+
+
+
+
+
 
 
 
